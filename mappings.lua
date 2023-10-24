6f8259ca9e05a3b58bc0660a9aed5767ab15fb6e
osch88 <kristiansson.oscar@gmail.com>
2023-10-24 20:42:42 +0200

    Fix lsp etc

--- mappings.lua
+++ mappings.lua
@@ -8,10 +8,10 @@ return {
   n = {
     -- second key is the lefthand side of the map
     -- vim.api.nvim_set_keymap('n', '<leader>oo', ':ClangdSwitchSourceHeader<cr>', { silent = true })
-    ["<leader>Oo"] = { ":ClangdSwitchSourceHeader<cr>", desc = "Switch to header/source" },
+    ["<leader>äo"] = { ":ClangdSwitchSourceHeader<cr>", desc = "Switch to header/source" },
 
     -- vim.keymap.set('n', '<leader>ob', "<cmd>e #<cr>", { noremap = true, silent = true }) -- go back to last buffer
-    ["<leader>Ob"] = { "<cmd>e #<cr>", desc = "Go back to previous buffer" },
+    ["<leader>äb"] = { "<cmd>e #<cr>", desc = "Go back to previous buffer" },
 
     -- navigate buffer tabs with `H` and `L`
     L = {
